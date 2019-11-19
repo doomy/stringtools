bdbac84301665ecc36c7f4b2ec9590bd39013862
Vladimir Bartek <vladimir.bartek@gmail.com>
2019-11-19 18:23:23 +0100

    Stringtools library

---
+++ src/StringTools.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Doomy\Helper;
+
+class StringTools
+{
+    public static function normalizeStringForUri($string) {
+        $urlEncoded = urlencode(static::removeCzDiacritics($string));
+        return str_replace('+', '-', $urlEncoded);
+    }
+
+    public static function underscoresToCamelCase($string, $capitalizeFirstCharacter = false)
+    {
+
+
+        $str = str_replace('_', '', ucwords(strtolower($string), '_'));
+
+        if (!$capitalizeFirstCharacter) {
+            $str = lcfirst($str);
+        }
+
+        return $str;
+    }
+
+    public static function isAllCaps(string $string): bool
+    {
+        return strtoupper($string) == $string;
+    }
+
+    private static function removeCzDiacritics($string) {
+        $conversionTable = json_decode(file_get_contents(__DIR__ . "../resources/czCharConversionMap.json"), TRUE);
+        return strtr($string, $conversionTable);
+    }
+}
